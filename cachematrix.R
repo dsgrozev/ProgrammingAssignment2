384d361f045c89d85dbc055e733e60f177306c92
Dimitar Grozev <dsgrozev@hotmail.com>
2016-03-01 14:01:46 -0800

    Caching the Inverse of a Matrix
    Matrix inversion is usually a costly computation and there may be some
    benefit to caching the inverse of a matrix rather than compute it
    repeatedly.
    
    Functions:
    1.makeCacheMatrix: This function creates a special "matrix" object that
    can cache its inverse.
    2.cacheSolve: This function computes the inverse of the special "matrix"
    returned by makeCacheMatrix above. If the inverse has already been
    calculated (and the matrix has not changed), then the cachesolve will
    retrieve the inverse from the cache.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is usually a costly computation and there may be some 
+## benefit to caching the inverse of a matrix rather than computing it 
+## repeatedly. 
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    cached <- NULL
+    set <- function (y)
+    {
+        x <<- y
+        cached <<- NULL
+    }
+    get <- function() x
+    setSolved <- function(solve) cached <<- solve
+    getSolved <- function() cached
+    list(set = set, get = get, setSolved = setSolved, 
+         getSolved = getSolved)
+    
 }
 
-
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by
+## makeCacheMatrix above. If the inverse has already been calculated (and the 
+## matrix has not changed), then cacheSolve retrieves the inverse from the 
+## cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    cached <- x$getSolved()
+    if(!is.null(cached))
+    {
+        message("getting cached data")
+        return (cached)
+    }
+    data <- x$get()
+    cached <- solve(data, ...)
+    x$setSolved(cached)
+    cached
 }
